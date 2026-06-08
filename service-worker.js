fe35052e0f7ab4f329eb67b3f81b9bb9ea3a7aa6
vabhi2024 <vabhi2024@gmail.com>
2026-06-08 20:17:40 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'a1market-v1';
+const urlsToCache = ['/', 'index.html', 'style.css', 'script.js', 'favicon.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
